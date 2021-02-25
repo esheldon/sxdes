610c7df55d95e87c32535475257a981b2851d3ac
Erin Sheldon <erin.sheldon@gmail.com>
2021-02-25 08:32:38 -0500

    use setuptools

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name="sxdes",
+    description="run the sep code on images with DES settings",
+    version="0.1.0",
+    packages=find_packages(),
+    license="GPL",
     author="Erin Sheldon",
     url="https://github.com/esheldon/sxdes",
-    description="run the sep code on imags with DES settings",
-    packages=['sxdes'],
-    version="0.1",
 )
