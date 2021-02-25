345f900e05dd707621638dc3cfe1c12e84f803e4
Erin Sheldon <erin.sheldon@gmail.com>
2021-02-25 08:49:23 -0500

    add tests

---
+++ sxdes/tests/test_detect.py
@@ -0,0 +1,51 @@
+import numpy as np
+import sxdes
+
+
+def _make_image(rng):
+    dims = 32, 32
+    sigma = 2.0
+    counts = 100.0
+    noise = 0.1
+
+    cen = (np.array(dims)-1)/2
+    cen += rng.uniform(low=-2, high=2, size=2)
+
+    rows, cols = np.mgrid[
+        0: dims[0],
+        0: dims[1],
+    ]
+
+    rows = rows - cen[0]
+    cols = cols - cen[1]
+
+    norm = 1.0/(2 * np.pi * sigma**2)
+
+    image = np.exp(-0.5*(rows**2 + cols**2)/sigma**2)
+    image *= counts*norm
+
+    # import hickory
+    # plt = hickory.Plot(aratio=1)
+    # plt.imshow(image)
+    # plt.show()
+    return image, noise, cen
+
+
+def test_detect_smoke():
+    rng = np.random.RandomState(646509750)
+    image, noise, _ = _make_image(rng)
+    _ = sxdes.run_sep(image, noise)
+
+
+def test_detect():
+    rng = np.random.RandomState(60970)
+
+    for i in range(10):
+        image, noise, cen = _make_image(rng)
+        cat, seg = sxdes.run_sep(image, noise)
+        assert cat.size > 0
+
+        s = cat['flux'].argsort()[-1]
+        row, col = cat['y'][s], cat['x'][s]
+        assert abs(row - cen[0]) < 1
+        assert abs(col - cen[1]) < 1
